b0cb787749b40dcf6b78feb7aaeeff2e0a909272
Sakthidharan Jagadeesan <sakthidharan.j@thoughtworks.com>
2022-05-10 23:45:37 +0530

    Refactoring - 'feature envy' code smell removed by moving method 'amount' from Customer class to its appropriate class Rental

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -28,8 +28,8 @@ public class Customer {
 
             //show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" +
-                    amount(rental) + "\n";
-            totalAmount += amount(rental);
+                    rental.amount() + "\n";
+            totalAmount += rental.amount();
         }
 
         //add footer lines result
@@ -37,27 +37,6 @@ public class Customer {
         result += "You earned " + frequentRenterPoints
                 + " frequent renter points";
         return result;
-    }
-
-    private double amount(Rental rental) {
-        double thisAmount = 0;
-        //determine amounts for each line
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented() > 2)
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (rental.getDaysRented() > 3)
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
     }
 
     public String htmlStatement() {
--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -26,4 +26,25 @@ public class Rental {
                 getDaysRented() > 1) frequentRenterPoints++;
         return frequentRenterPoints;
     }
+
+    public double amount() {
+        double thisAmount = 0;
+        //determine amounts for each line
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2)
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3)
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
