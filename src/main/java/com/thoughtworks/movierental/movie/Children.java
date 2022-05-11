103f36006eaadb4e666f53bee6c0cea56eb49cf1
Sakthidharan Jagadeesan <sakthidharan.j@thoughtworks.com>
2022-05-12 00:41:44 +0530

    Refactoring - combined addition statements in Children amount computation

--- src/main/java/com/thoughtworks/movierental/movie/Children.java
+++ src/main/java/com/thoughtworks/movierental/movie/Children.java
@@ -1,14 +1,13 @@
 package com.thoughtworks.movierental.movie;
 
-public class Children extends Movie{
+public class Children extends Movie {
     public Children(String title) {
         super(title, Movie.CHILDREN);
     }
 
     @Override
     public double amount(int daysRented) {
-        double thisAmount = super.amount(daysRented);
-        thisAmount += 1.5;
+        double thisAmount = super.amount(daysRented) + 1.5;
         if (daysRented > 3)
             thisAmount += (daysRented - 3) * 1.5;
         return thisAmount;
