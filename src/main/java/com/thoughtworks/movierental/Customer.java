2c8a211f44e529d13071130f6980ef811e995a26
Sakthidharan Jagadeesan <sakthidharan.j@thoughtworks.com>
2022-05-11 21:00:27 +0530

    Refactoring - removed 'Dead Code'

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -26,35 +26,8 @@ public class Customer {
         return this.textStatement.generate();
     }
 
-    private String footer() {
-        return "Amount owed is " + rentals.totalAmount() + "\n"
-                + "You earned " + rentals.frequentRenterPoints()
-                + " frequent renter points";
-    }
-
-    private String body() {
-        return rentals.transform(rental ->
-                "\t" + rental.title() + "\t" + rental.amount() + "\n");
-    }
-
-    private String header() {
-        return "Rental Record for " + getName() + "\n";
-    }
-
     public String htmlStatement() {
         return htmlStatement.generate();
     }
 
-    private String htmlStatementBody() {
-        return rentals.transform(rental -> String.format("%s&ensp;%s<br>\n", rental.title(), rental.amount()));
-    }
-
-    private String htmlStatementHeader() {
-        return String.format("<h1>Rental Record for <b>%s</b></h1>\n", getName());
-    }
-
-    private String htmlStatementFooter() {
-        return String.format("Amount owed is <b>%s</b><br>\n", rentals.totalAmount()) +
-                String.format("You earned <b>%s</b> frequent renter points<br>\n", rentals.frequentRenterPoints());
-    }
 }
